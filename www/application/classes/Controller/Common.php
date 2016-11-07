f70cf285d92898b1cfff0107ede2e2eed0fe5a76
Polina <tsareva_polina@mail.ru>
2016-11-07 04:45:26 +0300

    правки

--- www/application/classes/Controller/Common.php
+++ www/application/classes/Controller/Common.php
@@ -1,10 +1,10 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller_Template {
+class Controller_Common extends Controller_Template {
  
     public function action_index()
     {
-        $content = View::factory('welcome')
+        $content = View::factory('common')
               ->bind('age', $age);
         $age = 'больше 18';
         $content->name = 'Васька';
